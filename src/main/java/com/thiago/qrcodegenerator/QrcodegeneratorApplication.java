2faab7e6c371042f7ebc474514f2bdb81e23797e
Thiago Bueno <thiagobuenogoncalves@gmail.com>
2025-05-01 18:23:12 -0300

    Primeiro commit

---
+++ src/main/java/com/thiago/qrcodegenerator/QrcodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/qrcodegenerator/QrcodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
